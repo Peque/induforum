b39503fa201ce80b5a14b715f45e741ebf38b0ad
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2012-12-10 04:02:29 +0100

    Fixed languages form

--- www/data/content/students/participate/languages.en.php
+++ www/data/content/students/participate/languages.en.php
@@ -51,47 +51,55 @@
 			$query = "delete from languages where student_number='".$student_number."'";
 			mysqli_query($db, $query);
 
+		$writing_success = 1;
+
+		$num_results = mysqli_real_escape_string($db, trim($_POST['languages']));
+
 		// Form data overrides any other data
-		for ($j=1;$j<=$_POST['languages'];$j++){
-		$spd['language'] = mysqli_real_escape_string($db, trim($_POST['language'.$j]));
-		$spd['listening'] = mysqli_real_escape_string($db, trim($_POST['listening'.$j]));
-		$spd['reading'] = mysqli_real_escape_string($db, trim($_POST['reading'.$j]));
-		$spd['spoken_interaction'] = mysqli_real_escape_string($db, trim($_POST['spoken_interaction'.$j]));
-		$spd['spoken_production'] = mysqli_real_escape_string($db, trim($_POST['spoken_production'.$j]));
-		$spd['writing'] = mysqli_real_escape_string($db, trim($_POST['writing'.$j]));
-		// Check if all fields have a non-empty value
-		if ($spd['language'] != "" &&
-			$spd['listening'] != "" &&
-			$spd['reading'] != "" &&
-			$spd['spoken_interaction'] != "" &&
-			$spd['spoken_production'] != "" &&
-			$spd['writing'] != "") {
+		for ($j=0;$j<$num_results;$j++){
 
-			// Try to add a new row
-			$query = "insert into languages values
-									('".$student_number."',
-									'".$spd['language']."',
-									'".$spd['listening']."',
-									'".$spd['reading']."',
-									'".$spd['spoken_interaction']."',
-									'".$spd['spoken_production']."',
-									'".$spd['writing']."')";
+			$language[$j] = mysqli_real_escape_string($db, trim($_POST['language'.$j]));
+			$listening[$j] = mysqli_real_escape_string($db, trim($_POST['listening'.$j]));
+			$reading[$j] = mysqli_real_escape_string($db, trim($_POST['reading'.$j]));
+			$interaction[$j] = mysqli_real_escape_string($db, trim($_POST['spoken_interaction'.$j]));
+			$production[$j] = mysqli_real_escape_string($db, trim($_POST['spoken_production'.$j]));
+			$writing[$j] = mysqli_real_escape_string($db, trim($_POST['writing'.$j]));
+
+			// Check if all fields have a non-empty value
+			if ($language[$j] != "" &&
+				$listening[$j] != "" &&
+				$reading[$j] != "" &&
+				$interaction[$j] != "" &&
+				$production[$j] != "" &&
+				$writing[$j] != "") {
+
+				// Try to add a new row
+				$query = "insert into languages values
+										('".$student_number."',
+										'".$language[$j]."',
+										'".$listening[$j]."',
+										'".$reading[$j]."',
+										'".$interaction[$j]."',
+										'".$production[$j]."',
+										'".$writing[$j]."')";
 
 
-			$result = mysqli_query($db, $query);
-			// Inform the user about the operation
-			if ($result) echo '<p class="info">Data saved successfuly.</p>';
-			else echo '<p class="error"><strong>Error: </strong>could not write to the database. Please, try again later.</p>';
+				$result = mysqli_query($db, $query);
+				// Inform the user about the operation
+				if (!$result) $writing_success = 0;
 
-		} else {
+			} else {
 
-			// Need to fill more fields in the form
-			echo '<p class="warning">Please, fill all the required fields in the form!</p>';
+				// Need to fill more fields in the form
+				echo '<p class="warning">Please, fill all the required fields in the form!</p>';
+
+			}
 
 		}
 
+		if ($writing_success) echo '<p class="info">Data saved successfuly.</p>';
+		else echo '<p class="error"><strong>Error: </strong>could not write to the database. Please, try again later.</p>';
 
-	}
 	} else {
 
 		// Try to get data from the database
@@ -136,8 +144,8 @@
 								<option value="2" <?php if (isset($num_results)&&$num_results=="2") echo 'selected="selected"'?>>2 </option>
 								<option value="3" <?php if (isset($num_results)&&$num_results=="3") echo 'selected="selected"'?>>3 </option>
 							</select>
-							<label for="form_language1" class="singleline">Language<span class="form_required" title="This field is required">*</span></label>
-							<select name="language1" id="form_language1" class="singleline" required="required">
+							<label for="form_language0" class="singleline">Language<span class="form_required" title="This field is required">*</span></label>
+							<select name="language0" id="form_language0" class="singleline" required="required">
 								<option value=""></option>
 								<option value="english" <?php if (isset($language[0])&&$language[0]=="english") echo 'selected="selected"'?>>English</option>
 								<option value="french" <?php if (isset($language[0])&&$language[0]=="french") echo 'selected="selected"'?>>French</option>
@@ -149,8 +157,8 @@
 								<option value="dutch" <?php if (isset($language[0])&&$language[0]=="dutch") echo 'selected="selected"'?>>Dutch</option>
 								<option value="chinese" <?php if (isset($language[0])&&$language[0]=="chinese") echo 'selected="selected"'?>>Chinese</option>
 							</select>
-							<label for="spoken_interaction1" class="singleline">Spoken Interaction</label>
-							<select name="spoken_interaction1" id="spoken_interaction1" class="singleline">
+							<label for="spoken_interaction0" class="singleline">Spoken Interaction</label>
+							<select name="spoken_interaction0" id="spoken_interaction0" class="singleline">
 								<option value=""></option>
 								<option value="A1" <?php if (isset($interaction[0])&&$interaction[0]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($interaction[0])&&$interaction[0]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -159,8 +167,8 @@
 								<option value="C1" <?php if (isset($interaction[0])&&$interaction[0]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($interaction[0])&&$interaction[0]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="spoken_production1" class="singleline">Spoken Production</label>
-							<select name="spoken_production1" id="spoken_production1" class="singleline">
+							<label for="spoken_production0" class="singleline">Spoken Production</label>
+							<select name="spoken_production0" id="spoken_production0" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($production[0])&&$production[0]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($production[0])&&$production[0]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -169,8 +177,8 @@
 								<option value="C1" <?php if (isset($production[0])&&$production[0]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($production[0])&&$production[0]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="reading1" class="singleline">Reading</label>
-							<select name="reading1" id="reading1" class="singleline">
+							<label for="reading0" class="singleline">Reading</label>
+							<select name="reading0" id="reading0" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($reading[0])&&$reading[0]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($reading[0])&&$reading[0]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -179,8 +187,8 @@
 								<option value="C1" <?php if (isset($reading[0])&&$reading[0]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($reading[0])&&$reading[0]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="writing1" class="singleline">Writing</label>
-							<select name="writing1" id="writing1" class="singleline">
+							<label for="writing0" class="singleline">Writing</label>
+							<select name="writing0" id="writing0" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($writing[0])&&$writing[0]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($writing[0])&&$writing[0]=="A1") echo 'selected="selected"'?>>A2</option>
@@ -189,8 +197,8 @@
 								<option value="C1" <?php if (isset($writing[0])&&$writing[0]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($writing[0])&&$writing[0]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="listening1" class="singleline">Listening</label>
-							<select name="listening1" id="listening1" class="singleline">
+							<label for="listening0" class="singleline">Listening</label>
+							<select name="listening0" id="listening0" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($listening[0])&&$listening[0]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($listening[0])&&$listening[0]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -199,8 +207,8 @@
 								<option value="C1" <?php if (isset($listening[0])&&$listening[0]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($listening[0])&&$listening[0]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="form_language2" class="singleline">Language</label>
-							<select name="language2" id="form_language2" class="singleline" >
+							<label for="form_language1" class="singleline">Language</label>
+							<select name="language1" id="form_language1" class="singleline" >
 								<option value=""></option>
 								<option value="english" <?php if (isset($language[1])&&$language[1]=="english") echo 'selected="selected"'?>>English</option>
 								<option value="french" <?php if (isset($language[1])&&$language[1]=="french") echo 'selected="selected"'?>>French</option>
@@ -212,8 +220,8 @@
 								<option value="dutch" <?php if (isset($language[1])&&$language[1]=="dutch") echo 'selected="selected"'?>>Dutch</option>
 								<option value="chinese" <?php if (isset($language[1])&&$language[1]=="chinese") echo 'selected="selected"'?>>Chinese</option>
 							</select>
-							<label for="spoken_interaction2" class="singleline">Spoken Interaction</label>
-							<select name="spoken_interaction2" id="spoken_interaction2" class="singleline">
+							<label for="spoken_interaction1" class="singleline">Spoken Interaction</label>
+							<select name="spoken_interaction1" id="spoken_interaction1" class="singleline">
 								<option value=""></option>
 								<option value="A1" <?php if (isset($interaction[1])&&$interaction[1]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($interaction[1])&&$interaction[1]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -222,8 +230,8 @@
 								<option value="C1" <?php if (isset($interaction[1])&&$interaction[1]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($interaction[1])&&$interaction[1]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="spoken_production2" class="singleline">Spoken Production</label>
-							<select name="spoken_production2" id="spoken_production2" class="singleline">
+							<label for="spoken_production1" class="singleline">Spoken Production</label>
+							<select name="spoken_production1" id="spoken_production1" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($production[1])&&$production[1]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($production[1])&&$production[1]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -232,8 +240,8 @@
 								<option value="C1" <?php if (isset($production[1])&&$production[1]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($production[1])&&$production[1]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="reading2" class="singleline">Reading</label>
-							<select name="reading2" id="reading2" class="singleline">
+							<label for="reading1" class="singleline">Reading</label>
+							<select name="reading1" id="reading1" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($reading[1])&&$reading[1]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($reading[1])&&$reading[1]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -242,8 +250,8 @@
 								<option value="C1" <?php if (isset($reading[1])&&$reading[1]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($reading[1])&&$reading[1]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="writing2" class="singleline">Writing</label>
-							<select name="writing2" id="writing2" class="singleline">
+							<label for="writing1" class="singleline">Writing</label>
+							<select name="writing1" id="writing1" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($writing[1])&&$writing[1]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($writing[1])&&$writing[1]=="A1") echo 'selected="selected"'?>>A2</option>
@@ -252,8 +260,8 @@
 								<option value="C1" <?php if (isset($writing[1])&&$writing[1]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($writing[1])&&$writing[1]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="listening2" class="singleline">Listening</label>
-							<select name="listening2" id="listening2" class="singleline">
+							<label for="listening1" class="singleline">Listening</label>
+							<select name="listening1" id="listening1" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($listening[1])&&$listening[1]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($listening[1])&&$listening[1]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -262,8 +270,8 @@
 								<option value="C1" <?php if (isset($listening[1])&&$listening[1]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($listening[1])&&$listening[1]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="form_language3" class="singleline">Language</label>
-							<select name="language3" id="form_language3" class="singleline" >
+							<label for="form_language2" class="singleline">Language</label>
+							<select name="language2" id="form_language2" class="singleline" >
 								<option value=""></option>
 								<option value="english" <?php if (isset($language[2])&&$language[2]=="english") echo 'selected="selected"'?>>English</option>
 								<option value="french" <?php if (isset($language[2])&&$language[2]=="french") echo 'selected="selected"'?>>French</option>
@@ -275,8 +283,8 @@
 								<option value="dutch" <?php if (isset($language[2])&&$language[2]=="dutch") echo 'selected="selected"'?>>Dutch</option>
 								<option value="chinese" <?php if (isset($language[2])&&$language[2]=="chinese") echo 'selected="selected"'?>>Chinese</option>
 							</select>
-							<label for="spoken_interaction3" class="singleline">Spoken Interaction</label>
-							<select name="spoken_interaction3" id="spoken_interaction3" class="singleline">
+							<label for="spoken_interaction2" class="singleline">Spoken Interaction</label>
+							<select name="spoken_interaction2" id="spoken_interaction2" class="singleline">
 								<option value=""></option>
 								<option value="A1" <?php if (isset($interaction[2])&&$interaction[2]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($interaction[2])&&$interaction[2]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -285,8 +293,8 @@
 								<option value="C1" <?php if (isset($interaction[2])&&$interaction[2]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($interaction[2])&&$interaction[2]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="spoken_production3" class="singleline">Spoken Production</label>
-							<select name="spoken_production3" id="spoken_production3" class="singleline">
+							<label for="spoken_production2" class="singleline">Spoken Production</label>
+							<select name="spoken_production2" id="spoken_production2" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($production[2])&&$production[2]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($production[2])&&$production[2]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -295,8 +303,8 @@
 								<option value="C1" <?php if (isset($production[2])&&$production[2]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($production[2])&&$production[2]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="reading3" class="singleline">Reading</label>
-							<select name="reading3" id="reading3" class="singleline">
+							<label for="reading2" class="singleline">Reading</label>
+							<select name="reading2" id="reading2" class="singleline">
 											<option value="noreply"></option>
 								<option value="A1" <?php if (isset($reading[2])&&$reading[2]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($reading[2])&&$reading[2]=="A2") echo 'selected="selected"'?>>A2</option>
@@ -305,8 +313,8 @@
 								<option value="C1" <?php if (isset($reading[2])&&$reading[2]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($reading[2])&&$reading[2]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="writing3" class="singleline">Writing</label>
-							<select name="writing3" id="writing3" class="singleline">
+							<label for="writing2" class="singleline">Writing</label>
+							<select name="writing2" id="writing2" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($writing[2])&&$writing[2]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($writing[2])&&$writing[2]=="A1") echo 'selected="selected"'?>>A2</option>
@@ -315,8 +323,8 @@
 								<option value="C1" <?php if (isset($writing[2])&&$writing[2]=="C1") echo 'selected="selected"'?>>C1</option>
 								<option value="C2" <?php if (isset($writing[2])&&$writing[2]=="C2") echo 'selected="selected"'?>>C2</option>
 							</select>
-							<label for="listening3" class="singleline">Listening</label>
-							<select name="listening3" id="listening3" class="singleline">
+							<label for="listening2" class="singleline">Listening</label>
+							<select name="listening2" id="listening2" class="singleline">
 								<option value="noreply"></option>
 								<option value="A1" <?php if (isset($listening[2])&&$listening[2]=="A1") echo 'selected="selected"'?>>A1</option>
 								<option value="A2" <?php if (isset($listening[2])&&$listening[2]=="A2") echo 'selected="selected"'?>>A2</option>
