30a9786626271dc6a221fc2f98807d7f8fee453d
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2012-12-22 15:25:29 +0100

    Added sponsors script

--- www/js/sponsors.js
+++ www/js/sponsors.js
@@ -1,8 +1,75 @@
-// CAR-UPM
-document.write('<p style="text-align:center;"><a href="http://www.car.upm-csic.es/" title="Centro de Automática y Robótica (CAR UPM-CSIC)"><img src="ftp://reset.etsii.upm.es/.pool/images/sponsors/car_extended.png" alt="car_logo" /></a></p>');
 
-// CEI-UPM
-document.write('<p style="text-align:center;"><a href="http://www.cei.upm.es/" title="Centro de Electrónica Industrial (CEI-UPM)"><img src="ftp://reset.etsii.upm.es/.pool/images/sponsors/cei_extended.png" alt="cei_logo" /></a></p>');
 
-// DII-ETSII-UPM
-document.write('<p style="text-align:center;"><a href="http://www.dii.etsii.upm.es/" title="División de Informática Industrial (ETSII-UPM)"><img src="ftp://reset.etsii.upm.es/.pool/images/sponsors/dii.png" alt="dii_logo" /></a></p>');
+var sponsors = [
+
+	{
+		company: "Accenture",
+		website: "http://www.accenture.com/",
+		image: "accenture"
+	},
+
+	{
+		company: "BBVA",
+		website: "http://www.bbva.com/",
+		image: "bbva"
+	},
+
+	{
+		company: "IBM",
+		website: "http://www.ibm.com/",
+		image: "ibm"
+	},
+
+	{
+		company: "L'Oréal",
+		website: "http://www.loreal.com/",
+		image: "l'oréal"
+	},
+
+	{
+		company: "OSRAM",
+		website: "http://www.osram.com/",
+		image: "osram"
+	},
+
+	{
+		company: "SEAT",
+		website: "http://www.seat.com/",
+		image: "seat"
+	},
+
+	{
+		company: "Siemens",
+		website: "http://www.siemens.com/",
+		image: "siemens"
+	}
+
+];
+
+function new_chosen_one() {
+	var repeated = 1;
+	while (repeated) {
+		var random = Math.floor(Math.random() * (sponsors.length));
+		repeated = 0;
+		for (var i = 0; i < chosen.length; i++) {
+			if (random == chosen[i]) {
+				repeated = 1;
+				break;
+			}
+		}
+		if (!repeated) return random;
+	}
+	return 0;
+}
+
+var chosen = new Array();
+
+chosen[0] = new_chosen_one();
+chosen[1] = new_chosen_one();
+chosen[2] = new_chosen_one();
+chosen[3] = new_chosen_one();
+
+
+for (var i = 0; i < chosen.length; i++) {
+	document.write('<p style="text-align:center;"><a href="' + sponsors[chosen[i]].website + '" title="' + sponsors[chosen[i]].company + '"><img src="/images/sponsors/' + sponsors[chosen[i]].image + '" alt="' + sponsors[chosen[i]].company + '_logo" /></a></p>');
+}
