21a49b7e7dfc1e8bf66dabaae1a9af2788e2dcdf
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2013-01-27 22:54:43 +0100

    Improved user interface

--- www/data/content/logout.en.php
+++ www/data/content/logout.en.php
@@ -5,6 +5,26 @@
 	$_SESSION = array();
 	session_destroy();
 
-	header('Location: /en/login/');
-
 ?>
+
+<section id="content">
+<header>
+	<hgroup>
+		<h1>Logged out</h1>
+	</hgroup>
+</header>
+<article>
+	<header>
+		<hgroup>
+			<h1 id="Acess_form">Your session has been closed</h1>
+		</hgroup>
+		<hr />
+	</header>
+	<p class="centered extra_margin"><img src="/images/marx-brothers.jpg" alt="happy_face.svg" /></p>
+	<p>See you soon!</p>
+</article>
+<footer>
+	<p class="section_title">Logged out</p>
+</footer>
+
+</section>
--- www/data/content/logout.es.php
+++ www/data/content/logout.es.php
@@ -5,6 +5,26 @@
 	$_SESSION = array();
 	session_destroy();
 
-	header('Location: /es/login/');
-
 ?>
+
+<section id="content">
+<header>
+	<hgroup>
+		<h1>Desconectado</h1>
+	</hgroup>
+</header>
+<article>
+	<header>
+		<hgroup>
+			<h1 id="Acess_form">Se ha cerrado tu sesión</h1>
+		</hgroup>
+		<hr />
+	</header>
+	<p class="centered extra_margin"><img src="/images/marx-brothers.jpg" alt="happy_face.svg" /></p>
+	<p>¡Hasta pronto!</p>
+</article>
+<footer>
+	<p class="section_title">Desconectado</p>
+</footer>
+
+</section>
