ad67e23ea223bfcc7a644e2ece930c2970468eb9
Coloma <colomagm@gmail.com>
2013-02-21 20:14:21 +0100

    First part for results

---
+++ www/data/content/companies/results.en.php
@@ -0,0 +1,33 @@
+<?php
+
+// Connect to the database
+	$db = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+
+	// Check for database connection errors
+	if (mysqli_connect_errno()) {
+
+		echo $err_db_connection_error;
+
+	}
+
+	if (!mysqli_set_charset($db, 'utf8')) {
+
+		echo $err_db_charset_error;
+
+	}
+// create variables
+
+$C_studies=$_POST['C_stuedies'];
+$C_higher_course=$_POST['C_higher_course'];
+$C_speciality=$_POST['C_speciality'];
+
+
+
+?>
+
+
+
+mysql>select * from students_academic_data where studies='$C_studies' AND higher_course=$C_higher_course AND speciality='$C_speciality';
+
+
+mysql> select * from students_academic_data where studies='industrialengineering' AND higher_course=5;
