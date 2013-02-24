4d99de799228a1b682067b89924894ee0e1cb4a8
Ruben Ciudad <ruben.ciudad@gmail.com>
2013-02-24 20:35:28 +0100

    Forms names updated

--- www/data/content/companies/database.en.php
+++ www/data/content/companies/database.en.php
@@ -23,9 +23,16 @@
 </header>
 <article>
 
+<<<<<<< HEAD
 	<form action="" method="post">
 		<fieldset>
 			<legend>CV Filter</legend>
+=======
+	<form action="/en/companies/results" method="post">
+			<fieldset>
+			<legend>Academic</legend>
+
+>>>>>>> 10c9a22... Forms names updated
 			<div class="form_wrapper">
 				<label for="degree" class="singleline">Degree:</label>
 				<select name="C_studies" id="degree" class="singleline">
@@ -42,6 +49,12 @@
 					<option value="chemicalgradeengineering">Chemical Engineering Grade</option>
 					<option value="energyengineering">Energy Engineering Grade</option>
 				</select>
+<<<<<<< HEAD
+=======
+
+				<br>
+
+>>>>>>> 10c9a22... Forms names updated
 				<label for="course" class="singleline">Higher course:</label>
 				<select name="C_higher_course" id="course" class="singleline">
 					<option value="">Default: Any course</option>
@@ -49,6 +62,12 @@
 					<option value="4" >4</option>
 					<option value="5" >5</option>
 				</select>
+<<<<<<< HEAD
+=======
+
+				<br>
+
+>>>>>>> 10c9a22... Forms names updated
 				<label for="speciality" class="singleline">Speciality:</label>
 					<select name="C_speciality" id="speciality" class="singleline">
 						<option value="">Default: Any speciality</option>
@@ -61,6 +80,7 @@
 						<option value="Chemical" >Industrial Chemistry</option>
 						<option value="Energetic" >Energetic Technics</option>
 						<option value="Manufacturing">Manufacturing</option>
+<<<<<<< HEAD
 					</select>
 
 
@@ -120,6 +140,107 @@
 				<input type="checkbox" name="Communications and networks" value="Communications and networks" id="Communications_and_networks">
 
 
+=======
+					   </select>
+					   </div>
+			</fieldset>
+
+
+			<fieldset>
+
+				<legend>Languages</legend>
+				
+				<div class="form_wrapper">
+				<input name="english" id="english" value="english" type="checkbox"/>
+				<label for="english" class="checkbox">English</label> <br>
+
+				<input type="checkbox" name="french" value="french" id="french"/>
+				<label for="french" class="checkbox">French</label> <br>
+
+				<input type="checkbox" name="german" value="german" id="german"/>
+				<label for="german" class="checkbox">German</label> <br>
+
+				<input type="checkbox" name="italian" value="italian" id="italian"/>
+				<label for="italian" class="checkbox">Italian</label> <br>
+
+				<input type="checkbox" name="portuguese" value="portuguese" id="portuguese"/>
+				<label for="portuguese" class="checkbox">Portuguese</label> <br>
+
+				<input type="checkbox" name="russian" value="russian" id="russian"/>
+				<label for="russian" class="checkbox">Russian</label> <br>
+
+				<input type="checkbox" name="swedish" value="swedish" id="swedish"/>
+				<label for="swedish" class="checkbox">Swedish</label> <br>
+
+				<input type="checkbox" name="dutch" value="dutch" id="dutch"/>
+				<label for="dutch" class="checkbox">Dutch</label> <br>
+
+				<input type="checkbox" name="chinese" value="chinese" id="chinese"/>
+				<label for="chinese" class="checkbox">Chinese</label> <br>
+
+				</div>
+			</fieldset>
+
+
+			<fieldset>
+
+				<legend>Computing</legend>
+			
+				<div class="form_wrapper">
+				<input type="checkbox" name="Windows" value="SO Windows" id="SO_Windows"/>
+			    <label for="SO_Windows" class="checkbox">SO Windows:</label> <br>
+
+				<input type="checkbox" name="Mac" value="SO Mac" id="SO_Mac"/>
+     			<label for="SO_Mac" class="checkbox">SO Mac</label> <br>
+
+				<input type="checkbox" name="Linux" value="SO Linux" id="SO_Linux"/>
+				<label for="SO_Linux" class="checkbox">SO Linux</label> <br>
+
+
+				<input type="checkbox" name="Databases" value="Data bases" id="Data_bases"/>
+
+				<input type="checkbox" name="accounting/" value="Accounting/Finances" id="Accounting/Finances"/>
+				<label for="Accounting/Finances" class="checkbox">Accounting/Finances</label> <br>
+
+				<input type="checkbox" name="cad" value="Computer-aided design" id="Computer-aided_design"/>
+				<label for="Computer-aided_desig" class="checkbox">Computer-aided desig</label> <br>
+
+
+				<input type="checkbox" name="Graphic" value="Graphic Design" id="Graphic_Design"/>
+				<label for="Graphic_Design" class="checkbox">Graphic design</label> <br>
+
+				<input type="checkbox" name="Spreadsheet" value="Spreadsheet" id="Spreadsheet"/>
+
+				<input type="checkbox" name="Email" value="Email" id="Email">
+				<label for="Email" class="checkbox">Email</label> <br>
+
+
+				<input type="checkbox" name="Mathematics" value="Mathematics/Statistics" id="Mathematics/Statistics"/>
+				<label for="Mathematics/Statistics" class="checkbox">Mathematics/Statistics</label> <br>
+
+				<input type="checkbox" name="Presentations" value="Presentations" id="Presentations"/>
+				<label for="Presentations" class="checkbox">Presentations</label> <br>
+
+				<input type="checkbox" name="Wordprocessors" value="Word processors" id="Word_processors"/>
+				<label for="Word_processors" class="checkbox">Word processors</label> <br>
+
+
+				<input type="checkbox" name="Programation" value="Programation" id="Programation"/>
+				<label for="Programation" class="checkbox">Programation</label> <br>
+
+				<input type="checkbox" name="Simulation" value="Simulation" id="Simulation"/>
+
+				<input type="checkbox" name="Communications" value="Communications and networks" id="Communications_and_networks"/>
+				<label for="Communications_and_networks" class="checkbox">Communications and networks</label> <br>
+
+			</div>
+			</fieldset>
+
+
+			<fieldset>
+				<legend>Proffessional experience</legend>
+				<div class="form_wrapper">
+>>>>>>> 10c9a22... Forms names updated
 				<label for="work_time" class="singleline">Work activity:</label>
 				<select name="C_work_time" id="work_time" class="singleline">
 					<option value="">Default: Any</option>
@@ -129,6 +250,7 @@
 					<option value="4" >2 year or less</option>
 					<option value="3" >More than 2 years</option>
 				</select>
+<<<<<<< HEAD
 
 
 
@@ -143,6 +265,23 @@
 			</div>
 		</fieldset>
 		<input type="submit" value="Search" accesskey="x" />
+=======
+				</div>
+				</fieldset>
+
+				<fieldset>
+				<legend>Driver license</legend>
+				<div class="form_wrapper">
+				<label for="driver_license" class="singleline">Driver license:</label>
+				<input type="radio" name="driver_license" value="Yes" />
+				<input type="radio" name="driver_license" value="No" />
+				</div>
+				</fieldset>
+
+			
+			<input type="submit" value="Search" accesskey="x"  />
+			
+>>>>>>> 10c9a22... Forms names updated
 	</form>
 </article>
 <footer>
